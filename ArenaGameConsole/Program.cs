ab7561d915cfcda49ce9d5168766865f5a58fa6b
StelaMileva <stu2301561019@uni-plovdiv.bg>
2024-05-25 20:38:52 +0300

    first commit

---
+++ ArenaGameConsole/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ArenaGameEngine
+{
+    // Mage class that uses fire magic and spends mana for powerful attacks
+    public class Mage : Hero
+    {
+        public Mage() : this("Default Mage") // By default, all mages are named 'Default Mage'
+        {
+
+        }
+
+        public Mage(string name) : base(name) // A named mage, we supply the name
+        {
+            Mana = 150; // Initial mana
+        }
+
+        public int Mana { get; private set; }
+
+        public override int Attack()
+        {
+            if (Mana >= 50) // Check if there is enough mana for a powerful attack
+            {
+                Mana -= 50; // Reduce mana after the attack
+                int damage = 80; // Define the damage from the fire spell
+                return damage;
+            }
+            else
+            {
+                Console.WriteLine($"{Name} doesn't have enough mana for a powerful attack. Using a weaker spell.");
+                return base.Attack(); // Use the base attack if there is not enough mana
+            }
+        }
+
+        public override void TakeDamage(int incomingDamage)
+        {
+            base.TakeDamage(incomingDamage); // Take damage as per the base Hero class
+        }
+    }
+}
---
+++ ArenaGameEngine/Joker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGameEngine
+{
+    // Joker class that focuses on deception and debuffs
+    public class Joker : Hero
+    {
+        public Joker() : this("Default Joker") // By default, all jokers are named 'Default Joker'
+        {
+
+        }
+
+        public Joker(string name) : base(name) // A named joker, we supply the name
+        {
+        }
+
+        public override int Attack()
+        {
+            // Joker distracts the opponent, making them vulnerable to debuffs
+            Console.WriteLine($"{Name} distracts {target.Name}, making them vulnerable to debuffs!");
+            return base.Attack(); // Use the base attack for simplicity
+        }
+
+        public void Debuff(Hero target)
+        {
+            // Joker inflicts debuffs on the opponent
+            Console.WriteLine($"{Name} inflicts debuffs on {target.Name}, making them weaker!");
+        }
+
+        public override void TakeDamage(int incomingDamage)
+        {
+            base.TakeDamage(incomingDamage); // Take damage as per the base Hero class
+        }
+    }
+}
